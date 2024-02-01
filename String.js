274f38ec2fc85f252868fa2777e183be04c87537
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-02-01 14:50:31 +0600

    String part completed

--- String.js
+++ String.js
@@ -9,6 +9,38 @@ console.log(typeof (division));
 const thana = new String('Sreemangal');
 console.log(typeof (thana));
 
+//Array
+//Array is mutable can change.
+const num = [58, 24, 98, 21, 90];
+console.log(num)
+console.log(num.length)
+
+//Set 01 index element
+num[1] = 100;
+console.log(num)
+console.log(num.length)
+
+//Get 04 index values
+console.log(num[4])
+
+//String values
+const name = 'Pappu';
+console.log(name)
+console.log(name.length)
+
+//String is immutable Can not change index element from string.
+name[0] = 'B';
+console.log(name)
+
+const name2 = 'Pap pu';
+console.log(name2)
+console.log(name2.length)
+
+
+
+//Get 04 index values
+console.log(name[4])
+
 
 
 
