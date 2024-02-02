4e37eeb8bc08b7abae7eaa02b4040365955b11b8
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-02-03 00:57:43 +0600

    Value & Keys practice done

---
+++ Keys.js
@@ -0,0 +1,14 @@
+// Keys
+
+const Student = {
+    name: 'Pappu',
+    id: '212-15-14749',
+    department: 'CSE',
+    universityName:'DIU'
+}
+
+// Find how meany  object key
+
+const keys = Object.keys(Student) 
+console.log(keys)
+
---
+++ Values.js
@@ -0,0 +1,13 @@
+// Value
+
+const Student = {
+    name: 'Pappu',
+    id: '212-15-14749',
+    department: 'CSE',
+    universityName:'DIU'
+}
+
+// Find object key value
+
+const Value = Object.values(Student) 
+console.log(Value)
